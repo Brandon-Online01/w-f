2415367fbac9818b396c05d4bd88aa0674d9ec2d
brandonOnline <brandonnkawu01@gmail.com>
2024-10-09 20:58:58 +0200

    ..

--- src/modules/inventory/highlight-cards.tsx
+++ src/modules/inventory/highlight-cards.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import { Code, Puzzle, Repeat, Zap } from "lucide-react";
+import { Code, PlugZap, Puzzle, Repeat, Warehouse, Wrench, Zap } from "lucide-react";
 import HighlightsCards from "@/components/highlight-cards";
 
 export const InventoryHighlights = () => {
     const highlights = [
-        { title: "Active Components", value: "23", Icon: Puzzle },
-        { title: "Total Components", value: "48", Icon: Zap, },
-        { title: "Deprecated Components", value: "48", Icon: Repeat, },
-        { title: "Needing Attention", value: "48", Icon: Code, },
+        { title: "Active Components", value: "23", Icon: PlugZap },
+        { title: "Active Moulds", value: "48", Icon: PlugZap, },
+        { title: "In Repair", value: "48", Icon: Wrench, },
+        { title: "Out of Service", value: "48", Icon: Warehouse, },
     ];
 
     return (
