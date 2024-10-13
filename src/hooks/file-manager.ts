8973c7a766cdff88bc28b1daeb8140cbbfbdbae5
brandonOnline <brandonnkawu01@gmail.com>
2024-10-13 18:36:27 +0200

    component creation and removal

--- src/hooks/file-manager.ts
+++ src/hooks/file-manager.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 export const CloudFileManager = () => {
-    const uploadMedia = async (file: ImageFile, config: {}): Promise<{ uploaded: boolean, newFileName: string | null }> => {
+    const uploadMedia = async (file: ImageFile): Promise<{ uploaded: boolean, newFileName: string | null }> => {
         if (typeof file === 'string' && /\.(png|jpe?g|svg|mp4|ico)$/i.test(file)) {
             return { uploaded: true, newFileName: file };
         }
@@ -15,7 +15,7 @@ export const CloudFileManager = () => {
                 const formData = new FormData();
                 formData.append('file', file as any)
 
-                const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, formData, config)
+                const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, formData)
 
                 if (response?.data && response?.data?.message === 'Media uploaded successfully' && response?.data?.newFileName) {
 
@@ -58,7 +58,7 @@ export const CloudFileManager = () => {
         }
     }
 
-    const deleteMedia = async (title: string, config: {}): Promise<{ deleted: boolean, message: string | null }> => {
+    const deleteMedia = async (title: string): Promise<{ deleted: boolean, message: string | null }> => {
         if (typeof title !== 'string' && !/\.(png|jpe?g|svg|mp4|ico)$/i.test(title)) {
             return { deleted: true, message: 'file' };
         }
@@ -66,7 +66,7 @@ export const CloudFileManager = () => {
             try {
                 const url = `files/delete/${title}`;
 
-                const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, config)
+                const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`)
 
                 if (response?.data && response?.data?.message === 'File deleted successfully') {
                     const result = {
