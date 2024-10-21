ab5f07313ba7bf6da75ec2d581806483115e169d
brandonOnline <brandonnkawu01@gmail.com>
2024-10-21 21:31:11 +0200

    updates refs

--- src/modules/live/highlights/index.tsx
+++ src/modules/live/highlights/index.tsx
@@ -65,7 +65,7 @@ export default function LiveRunHighlights() {
             });
 
             socket.on('connect', () => {
-                //
+                console.log('connected to live stream');
             });
 
             socket.on('highlights', (data) => {
