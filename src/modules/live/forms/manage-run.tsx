eb890d8d803546ad4a6d437ae8a77a840e273d5e
brandonOnline <brandonnkawu01@gmail.com>
2024-10-31 12:20:11 +0200

    ..

--- src/modules/live/forms/manage-run.tsx
+++ src/modules/live/forms/manage-run.tsx
@@ -55,7 +55,6 @@ export default function ManagementTab({ liveRun }: { liveRun: MachineLiveRun }) 
         setAllUsers,
         setAllComponents,
         setAllMoulds,
-        allUsers,
         allComponents,
         allMoulds
     } = liveRunStore();
